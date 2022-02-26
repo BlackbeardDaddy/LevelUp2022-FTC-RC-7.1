dba086acacc0d3376a079eb34c1d3df333e0963f
ProfLS <43427862+BlackbeardDaddy@users.noreply.github.com>
2022-02-25 20:29:07 -0800

    Meow

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/LaserTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/LaserTest.java
@@ -4,16 +4,18 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.TOF10120;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.TOF10120;
 
+@TeleOp
 public class LaserTest extends LinearOpMode {
 
     private TOF10120 Laser;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Laser = hardwareMap.get(TOF10120.class, "tempSensor");
+        Laser = hardwareMap.get(TOF10120.class, "Laser");
 
         waitForStart();
 
